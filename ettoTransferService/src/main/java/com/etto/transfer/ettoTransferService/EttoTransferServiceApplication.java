d53f6116424a4a4155da4c5ce9c2eadc5c0f8e7d
creatorarnea <creatorarena@gmail.com>
2020-12-17 02:56:31 +0900

    add on spring boot project

---
+++ ettoTransferService/src/main/java/com/etto/transfer/ettoTransferService/EttoTransferServiceApplication.java
@@ -0,0 +1,13 @@
+package com.etto.transfer.ettoTransferService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EttoTransferServiceApplication {
+
+	public static void main(String[] args){
+		SpringApplication.run(EttoTransferServiceApplication.class, args);
+		
+	}
+}
---
+++ ettoTransferService/src/test/java/com/etto/transfer/ettoTransferService/EttoTransferServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etto.transfer.ettoTransferService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EttoTransferServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
