e7c0bfde4a40c1e7565ca4f2fdf15bee26c7aaff
creatorarnea <creatorarena@gmail.com>
2020-12-17 23:06:01 +0900

    add war packaging build script, war packaging commit

--- ettoTransferService/src/main/java/com/etto/transfer/ettoTransferService/EttoTransferServiceApplication.java
+++ ettoTransferService/src/main/java/com/etto/transfer/ettoTransferService/EttoTransferServiceApplication.java
@@ -2,12 +2,19 @@ package com.etto.transfer.ettoTransferService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EttoTransferServiceApplication {
+public class EttoTransferServiceApplication extends SpringBootServletInitializer{
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		SpringApplication.run(EttoTransferServiceApplication.class, args);
-		
+		System.out.println("hello aws");
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(EttoTransferServiceApplication.class);
 	}
 }
